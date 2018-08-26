ea8bbf2be969f2d8055efeeeb14f17507bbbe6ad
Gowtham Karnan <gowthamkarnan.prg@gmail.com>
2018-08-27 00:24:24 +0530

    Format timestamp using moment library

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,15 +1,17 @@
+var moment = require('moment');
+
 var generateMessage = (from,text)=>{
     return {
         from,
         text,
-        creadtedAt: new Date().getTime()
+        creadtedAt: moment().valueOf()
     }
 };
 var generateLocationMessage = (from,latitude,longitude)=>{
     return{
         from,
         url:`https://www.google.com/maps?q=${latitude},${longitude}`,
-        creadtedAt: new Date().getTime()
+        creadtedAt: moment().valueOf()
     };
 };
 module.exports = {generateMessage,generateLocationMessage};
